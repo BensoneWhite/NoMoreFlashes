67410b12b4ab093279bec2b1c4dc649dbce3f348
BensoneWhite <145090119+BensoneWhite@users.noreply.github.com>
2025-03-29 20:01:53 -0600

    Updating references

--- src/Global.cs
+++ src/Global.cs
@@ -5,4 +5,5 @@ global using BepInEx;
 global using BepInEx.Logging;
 global using Menu.Remix.MixedUI;
 
+#pragma warning disable CS0618 // Type or member is obsolete
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
